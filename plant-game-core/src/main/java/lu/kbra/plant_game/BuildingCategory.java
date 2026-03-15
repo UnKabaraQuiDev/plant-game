528a06360da9042df4c70c423b2f3eef5ff59566
UnKabaraQuiDev <66266372+UnKabaraQuiDev@users.noreply.github.com>
2026-03-15 09:51:29 +0100

    Reworked logo

--- plant-game-core/src/main/java/lu/kbra/plant_game/BuildingCategory.java
+++ plant-game-core/src/main/java/lu/kbra/plant_game/BuildingCategory.java
@@ -4,10 +4,15 @@ import lu.kbra.plant_game.engine.data.locale.Localizable;
 import lu.kbra.plant_game.engine.entity.ui.impl.IndexOwner;
 import lu.kbra.plant_game.generated.ColorMaterial;
 
-public interface BuildingCategory extends IndexOwner, Localizable {
+public interface BuildingCategory extends IndexOwner, Localizable/* , Comparable<BuildingCategory> */ {
 
 	String BUILDING_CATEGORY_KEY = "building.category.";
 
 	ColorMaterial getAccentColor();
+
+//	@Override
+//	default int compareTo(final BuildingCategory o) {
+//		return Integer.compare(this.getIndex(), o.getIndex());
+//	}
 
 }
